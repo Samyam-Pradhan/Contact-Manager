b8672a75aa9c8910bd0d62d50e2a4d78d2f2a3be
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-01-11 14:35:01 +0545

    Used props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import AddContact  from './AddContact';
+import ContactList from './ContactList';
 
 
 function App(){
+    const contacts = [
+        {
+        id:1,
+        name:"Samyam",
+        email:"samyam123@gmail.com"
+        },
+        {
+            id:2,
+            name:"Binil",
+            email:"binil23@gmail.com"
+            },
+    ];
     return(
-        <div>
+        <div className='ui container'>
         <Header />
-</div>
+        <AddContact />
+        <ContactList contacts ={contacts} />
+        </div>
     );
 }
 export default App;
